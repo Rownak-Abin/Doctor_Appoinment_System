f3b471d9648057745a434c824b10f9ad3c523c7c
Rownak-Abin <62964382+Rownak-Abin@users.noreply.github.com>
2020-04-10 21:49:32 +0600

    Update PatientList.php

--- Views/PatientList.php
+++ Views/PatientList.php
@@ -1,29 +1,68 @@
 <html>
 	<head>
-		<link rel="stylesheet" type="text/css" href="PatientListSty.css">
+		<link rel="stylesheet" type="text/css" href="IdDocSty.css">
 	</head>
 	
 	
 	<body>
-		<div class="bg-model">
-			<div class="Contain">
-			Name: Abir Islam<br>
-			Phone: 01986698874<br>
-			Date: 22 january, 2020. Time:2pm<br><hr>		
+		<div style="position:absolute; left:300px; top:30px; width:1100px;">
 			
-			Name: Fahim islam<br>
-			Phone: 01746654874<br>
-			Date: 22 january, 2020. Time:3pm<br><hr>
-			
-			Name: Harun Rahman<br>
-			Phone: 01874698874<br>
-			Date: 22 january, 2020. Time:4pm<br><hr>
-			
-			Name: Anis Rahman<br>
-			Phone: 01636898874<br>
-			Date: 22 january, 2020. Time:5pm<br><hr>
+				<?php
+				
+				require_once "../Model/db_connect.php";
+					
+					if(isset($_GET['id'])){
+						$id = $_GET['id'];
+					}
+					
+					
+					$query="SELECT * FROM schedule WHERE id=$id";
+					
+					$list= gettwo($query);
+					
+					foreach($list as $ls){
+						echo "<div style='color:#000; font-size:17px;'>";
+						echo "Name: ";
+						echo " $ls[patname]; <br><br>";
+						echo "Email: ";
+						echo "$ls[email];<br><br> ";
+						echo "Appointment Date: ";
+						echo "$ls[date]  $ls[month], $ls[year]; <br><br>";
+						echo "Time: ";
+						echo "$ls[time]<br><br><hr><hr>";
+						echo "</div>";
+						
+						
+					}
+					
+					
 	
+				
+				?>
 			</div>
+			
+		
+			
+
+		
+		<div class="Sidenav" >
+	
+			<nav>
+				
+				
+				
+				<ul>
+					<li><a class="active" href="hos.php">Home</a></li>
+					<li><a href="#">News</a></li>
+					<li><a href="#">Contact us</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="Login.php"> Login</a></li>
+				
+				</ul>
+
+	
+			</nav>
+
 		</div>
 	
 	</body>
